828de8474bbc6fbf95c9ef826ae86ef0700439af
Bert Belder <bertbelder@gmail.com>
2017-09-01 14:10:36 +0200

    ntapi.h: remove DUMMYUNIONNAME annotation

--- src/ntapi.h
+++ src/ntapi.h
@@ -4062,7 +4062,7 @@ typedef struct _IO_STATUS_BLOCK {
   union {
     NTSTATUS Status;
     PVOID Pointer;
-  } DUMMYUNIONNAME;
+  };
   ULONG_PTR Information;
 } IO_STATUS_BLOCK, *PIO_STATUS_BLOCK;
 
