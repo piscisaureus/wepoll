45afb2268e2e32c3806063eba20850933396273f
Bert Belder <bertbelder@gmail.com>
2017-09-04 07:27:11 +0200

    util: add container_of() macro

--- src/util.h
+++ src/util.h
@@ -1,11 +1,16 @@
 #ifndef EPOLL_UTIL_H_
 #define EPOLL_UTIL_H_
 
+#include <stddef.h>
+
 #ifndef _SSIZE_T_DEFINED
 #define SSIZE_T_DEFINED
 typedef intptr_t ssize_t;
 #endif
 
 #define array_count(a) (sizeof(a) / (sizeof((a)[0])))
+
+#define container_of(ptr, type, member) \
+  ((type*) ((char*) (ptr) -offsetof(type, member)))
 
 #endif /* EPOLL_UTIL_H_ */
