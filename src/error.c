07b5e727a3128f3be6f7d10c168fa33e3ac4b0ea
Bert Belder <bertbelder@gmail.com>
2017-09-09 03:38:37 +0200

    error: avoid clang signed/unsigned comparison warning

--- src/error.c
+++ src/error.c
@@ -13,7 +13,7 @@ DWORD we_map_ntstatus_to_win_error(NTSTATUS status) {
   if (NT_SUCCESS(status))
     return ERROR_SUCCESS;
 #define X(ntstatus, win_error, win_errno, ws_error, ws_errno) \
-  if (status == ntstatus && win_error != -1)                  \
+  if (status == (NTSTATUS) ntstatus && win_error != -1)       \
     return (DWORD) win_error;
   WE_ERROR_MAP(X)
 #undef X
@@ -22,7 +22,7 @@ DWORD we_map_ntstatus_to_win_error(NTSTATUS status) {
 
 DWORD we_map_ntstatus_to_ws_error(NTSTATUS status) {
 #define X(ntstatus, win_error, win_errno, ws_error, ws_errno) \
-  if (status == ntstatus && ws_error != -1)                   \
+  if (status == (NTSTATUS) ntstatus && ws_error != -1)        \
     return (DWORD) ws_error;
   WE_ERROR_MAP(X)
 #undef X
@@ -31,9 +31,9 @@ DWORD we_map_ntstatus_to_ws_error(NTSTATUS status) {
 
 errno_t we_map_win_error_to_errno(DWORD error) {
 #define X(ntstatus, win_error, win_errno, ws_error, ws_errno) \
-  if (error == win_error && win_errno != -1)                  \
+  if (error == (DWORD) win_error && win_errno != -1)          \
     return win_errno;                                         \
-  if (error == ws_error && ws_errno != -1)                    \
+  if (error == (DWORD) ws_error && ws_errno != -1)            \
     return ws_errno;
   WE_ERROR_MAP(X)
 #undef X
