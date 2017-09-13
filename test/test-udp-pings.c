b8a2e2ea8492d9cc10da1f71766f950a3c0a3174
Bert Belder <bertbelder@gmail.com>
2017-09-14 01:27:48 +0200

    test-udp-pings: increase socket count to 10k

--- test/test-udp-pings.c
+++ test/test-udp-pings.c
@@ -7,7 +7,7 @@
 #include "win.h"
 
 static const char PING[] = "PING";
-static const int NUM_PINGERS = 1000;
+static const int NUM_PINGERS = 10000;
 static const DWORD RUN_TIME = 10000;
 
 int main(int argc, char* argv[]) {
