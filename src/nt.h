4a0354a6d25599099ef573c326f830ffd207b59c
Bert Belder <bertbelder@gmail.com>
2017-09-15 20:06:32 +0200

    nt: add definitions for keyed event support

--- src/nt.h
+++ src/nt.h
@@ -19,21 +19,55 @@ typedef VOID(NTAPI* PIO_APC_ROUTINE)(PVOID ApcContext,
                                      PIO_STATUS_BLOCK IoStatusBlock,
                                      ULONG Reserved);
 
-#define NTDLL_IMPORT_LIST(X)         \
-  X(NTSTATUS,                        \
-    NTAPI,                           \
-    NtDeviceIoControlFile,           \
-    (HANDLE FileHandle,              \
-     HANDLE Event,                   \
-     PIO_APC_ROUTINE ApcRoutine,     \
-     PVOID ApcContext,               \
-     PIO_STATUS_BLOCK IoStatusBlock, \
-     ULONG IoControlCode,            \
-     PVOID InputBuffer,              \
-     ULONG InputBufferLength,        \
-     PVOID OutputBuffer,             \
-     ULONG OutputBufferLength))      \
-  X(ULONG, WINAPI, RtlNtStatusToDosError, (NTSTATUS Status))
+typedef struct _LSA_UNICODE_STRING {
+  USHORT Length;
+  USHORT MaximumLength;
+  PWSTR Buffer;
+} LSA_UNICODE_STRING, *PLSA_UNICODE_STRING, UNICODE_STRING, *PUNICODE_STRING;
+
+typedef struct _OBJECT_ATTRIBUTES {
+  ULONG Length;
+  HANDLE RootDirectory;
+  PUNICODE_STRING ObjectName;
+  ULONG Attributes;
+  PVOID SecurityDescriptor;
+  PVOID SecurityQualityOfService;
+} OBJECT_ATTRIBUTES, *POBJECT_ATTRIBUTES;
+
+#define NTDLL_IMPORT_LIST(X)                                                 \
+  X(NTSTATUS,                                                                \
+    NTAPI,                                                                   \
+    NtDeviceIoControlFile,                                                   \
+    (HANDLE FileHandle,                                                      \
+     HANDLE Event,                                                           \
+     PIO_APC_ROUTINE ApcRoutine,                                             \
+     PVOID ApcContext,                                                       \
+     PIO_STATUS_BLOCK IoStatusBlock,                                         \
+     ULONG IoControlCode,                                                    \
+     PVOID InputBuffer,                                                      \
+     ULONG InputBufferLength,                                                \
+     PVOID OutputBuffer,                                                     \
+     ULONG OutputBufferLength))                                              \
+                                                                             \
+  X(ULONG, WINAPI, RtlNtStatusToDosError, (NTSTATUS Status))                 \
+                                                                             \
+  X(NTSTATUS,                                                                \
+    NTAPI,                                                                   \
+    NtCreateKeyedEvent,                                                      \
+    (PHANDLE handle,                                                         \
+     ACCESS_MASK access,                                                     \
+     POBJECT_ATTRIBUTES attr,                                                \
+     ULONG flags))                                                           \
+                                                                             \
+  X(NTSTATUS,                                                                \
+    NTAPI,                                                                   \
+    NtWaitForKeyedEvent,                                                     \
+    (HANDLE handle, PVOID key, BOOLEAN alertable, PLARGE_INTEGER mstimeout)) \
+                                                                             \
+  X(NTSTATUS,                                                                \
+    NTAPI,                                                                   \
+    NtReleaseKeyedEvent,                                                     \
+    (HANDLE handle, PVOID key, BOOLEAN alertable, PLARGE_INTEGER mstimeout))
 
 #define X(return_type, declarators, name, parameters) \
   EPOLL_INTERNAL_EXTERN return_type(declarators* name) parameters;
