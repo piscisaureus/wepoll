1e24f4423876d9e6b31d267365e75fcf3e9f8fdd
Bert Belder <bertbelder@gmail.com>
2017-09-25 17:51:43 +0200

    util: add safe_container_of() macro definition

---
+++ src/util.c
@@ -0,0 +1,11 @@
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "util.h"
+
+void* _util_safe_container_of_helper(void* ptr, size_t offset) {
+  if (ptr == NULL)
+    return NULL;
+  else
+    return (char*) ptr - offset;
+}
--- src/util.h
+++ src/util.h
@@ -3,12 +3,17 @@
 
 #include <stddef.h>
 
+#include "internal.h"
+
 #ifndef _SSIZE_T_DEFINED
 #define SSIZE_T_DEFINED
 typedef intptr_t ssize_t;
 #endif
 
 #define array_count(a) (sizeof(a) / (sizeof((a)[0])))
+
+#define safe_container_of(ptr, type, member) \
+  ((type*) _util_safe_container_of_helper((ptr), offsetof(type, member)))
 
 #define container_of(ptr, type, member) \
   ((type*) ((char*) (ptr) -offsetof(type, member)))
@@ -20,5 +25,7 @@ typedef intptr_t ssize_t;
 #define static_assert(condition, message) typedef __attribute__( \
     (unused)) int __static_assert_##__LINE__[(condition) ? 1 : -1];
 #endif
+
+EPOLL_INTERNAL void* _util_safe_container_of_helper(void* ptr, size_t offset);
 
 #endif /* EPOLL_UTIL_H_ */
