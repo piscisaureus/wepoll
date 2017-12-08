1ef60cb893e9cbfd93f44c82a2270c1fe2362923
Bert Belder <bertbelder@gmail.com>
2017-12-08 19:26:43 +0100

    win: ensure definitions for Vista or later are included

--- src/win.h
+++ src/win.h
@@ -5,6 +5,14 @@
 #define WIN32_LEAN_AND_MEAN
 #endif
 
+#if defined(_WIN32_WINNT) && _WIN32_WINNT < 0x0600
+#undef _WIN32_WINNT
+#endif
+
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0600
+#endif
+
 #pragma warning(push, 1)
 
 #include <windows.h>
