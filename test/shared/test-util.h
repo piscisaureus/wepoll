532d57ed31b7b8e2c6b2d820e1aa17dca8bac383
Bert Belder <bertbelder@gmail.com>
2020-05-09 11:03:35 +0200

    style: use clang-format version 10.x

--- test/shared/test-util.h
+++ test/shared/test-util.h
@@ -38,8 +38,9 @@ void no_inline no_return check_fail(const char* message);
 
 /* Polyfill `static_assert` for some versions of clang and gcc. */
 #if (defined(__clang__) || defined(__GNUC__)) && !defined(static_assert)
-#define static_assert(condition, message) typedef __attribute__( \
-    (__unused__)) int __static_assert_##__LINE__[(condition) ? 1 : -1]
+#define static_assert(condition, message) \
+  typedef __attribute__(                  \
+      (__unused__)) int __static_assert_##__LINE__[(condition) ? 1 : -1]
 #endif
 
 #endif /* TEST_UTIL_H_ */
